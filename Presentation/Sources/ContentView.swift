20217fe236a769f3fe41589cd8a5bb6e97365aab
신동규 <donggyu9410@gmail.com>
2025-08-14 01:48:59 +0900

    prepare font done

--- Presentation/Sources/ContentView.swift
+++ Presentation/Sources/ContentView.swift
@@ -5,29 +5,12 @@ public struct ContentView: View {
     public init() { }
     
     public var body: some View {
-        ScrollView {
-            VStack {
-                Text("안녕")
-                    .font(.pretendard(.bold, size: 40))
-                
-                Text("안녕")
-                    .font(.custom("asd", size: 40))
-                
-                Text("Available Font Families:")
-                    .font(.caption)
-                    .padding(.top)
-                
-                ForEach(UIFont.familyNames.sorted(), id: \.self) { familyName in
-                    VStack(alignment: .leading) {
-                        Text("\(familyName)")
-                            .font(.headline)
-                        ForEach(UIFont.fontNames(forFamilyName: familyName), id: \.self) { fontName in
-                            Text("  \(fontName)")
-                                .font(.caption)
-                        }
-                    }
-                }
-            }
+        VStack {
+            Text("안녕")
+                .font(.pretendard(.bold, size: 40))
+            
+            Text("안녕")
+                .font(.custom("asd", size: 40))
         }
     }
 }
