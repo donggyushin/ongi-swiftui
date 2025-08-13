cdac3f632a591242a10ce682c2aecd6c6754acfd
신동규 <donggyu9410@gmail.com>
2025-08-14 00:44:31 +0900

    Initial commit

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "ongi-swiftui",
+    targets: [
+        .target(
+            name: "ongi-swiftui",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.ongi-swiftui",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["ongi-swiftui/Sources/**"],
+            resources: ["ongi-swiftui/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "ongi-swiftuiTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.ongi-swiftuiTests",
+            infoPlist: .default,
+            sources: ["ongi-swiftui/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "ongi-swiftui")]
+        ),
+    ]
+)
---
+++ ongi-swiftui/Sources/OngiSwiftuiApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct OngiSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
