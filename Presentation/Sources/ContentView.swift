fa4a862b24cb82730961068b576dee61ce86ee67
신동규 <donggyu9410@gmail.com>
2025-08-14 01:50:49 +0900

    chore

--- Presentation/Sources/ContentView.swift
+++ Presentation/Sources/ContentView.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 public struct ContentView: View {
-    
+
     public init() { }
-    
+
     public var body: some View {
         VStack {
             Text("안녕")
                 .pretendardTitle1()
-            
+
             Text("안녕")
                 .font(.custom("asd", size: 40))
         }
