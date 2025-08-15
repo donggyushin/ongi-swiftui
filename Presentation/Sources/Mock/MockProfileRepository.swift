0a4e723debfacc9ec4e50083bd3345d09c2d4ea0
신동규 <donggyu9410@gmail.com>
2025-08-15 23:56:54 +0900

    feat: fetching initial image

--- Presentation/Sources/Mock/MockProfileRepository.swift
+++ Presentation/Sources/Mock/MockProfileRepository.swift
@@ -15,9 +15,10 @@ final class MockProfileRepository: PProfileRepository {
             id: "id",
             nickname: "달콤한레이지",
             email: "donggyu9410@gmail.com",
-            profileImage: .init(url: .init(string: "https://res.cloudinary.com/blog-naver-com-donggyu-00/image/upload/v1755242953/profile-images/profile_nickname_test_0023_1755242951793.jpg")!, publicId: "profile-images/profile_nickname_test_0023_1755242951793"),
+            profileImage: .init(url: .init(string: "https://res.cloudinary.com/blog-naver-com-donggyu-00/image/upload/v1755269648/profile-images/profile_nickname_test_0023_1755269630832.jpg")!, publicId: "profile-images/profile_nickname_test_0023_1755242951793"),
             images: [
-                .init(url: .init(string: "https://res.cloudinary.com/blog-naver-com-donggyu-00/image/upload/v1755242953/profile-images/profile_nickname_test_0023_1755242951793.jpg")!, publicId: "profile-images/profile_nickname_test_0023_1755242951793")
+                .init(url: .init(string: "https://res.cloudinary.com/blog-naver-com-donggyu-00/image/upload/v1755269648/profile-images/profile_nickname_test_0023_1755269630832.jpg")!, publicId: "profile-images/profile_nickname_test_0023_1755242951793"),
+                .init(url: .init(string: "https://res.cloudinary.com/blog-naver-com-donggyu-00/image/upload/v1755269702/profile-images/profile_gallery_nickname_test_0023_1755269701280.jpg")!, publicId: "profile-images/profile_nickname_test_0023_1755242951793")
             ],
             mbti: .infj,
             qnas: [
@@ -57,7 +58,7 @@ final class MockProfileRepository: PProfileRepository {
     init() { }
     
     func getMe(accessToken: String) async throws -> ProfileEntitiy {
-        profile2
+        profile1
     }
     
     func profileImageUpload(imageData: Data) async throws -> ProfileEntitiy {
