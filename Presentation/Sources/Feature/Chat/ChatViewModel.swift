7cf4dc33d1773c4d9a19c1d6eae6f9360a5f184f
Loopy <loopy@heyratel.com>
2025-08-22 18:16:21 +0900

    chore

--- Presentation/Sources/Feature/Chat/ChatViewModel.swift
+++ Presentation/Sources/Feature/Chat/ChatViewModel.swift
@@ -13,11 +13,27 @@ import Combine
 final class ChatViewModel: ObservableObject {
     let chatId: String
     
+    @Published var participants: [ProfileEntitiy] = []
+    @Published var messages: [MessageEntity] = []
+    @Published var loading = false
+    
+    var pagination: PaginationEntity?
+    
     @Injected(\.chatUseCase) private var chatUseCase
     
     init(chatId: String) {
         self.chatId = chatId
     }
     
-    
+    @MainActor
+    func fetchMessages() async throws {
+        loading = true
+        defer { loading = false }
+        
+        let result = try await chatUseCase.getChat(chatId: chatId, cursor: pagination?.nextCursor)
+        participants = result.0.participants
+        messages = result.0.messages
+        
+        pagination = result.1
+    }
 }
