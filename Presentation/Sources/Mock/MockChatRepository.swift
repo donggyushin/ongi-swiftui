676e9d50d0bc30ed07ed96a73d149cb29685a262
Loopy <loopy@heyratel.com>
2025-08-22 14:40:48 +0900

    chore

--- Presentation/Sources/Mock/MockChatRepository.swift
+++ Presentation/Sources/Mock/MockChatRepository.swift
@@ -8,7 +8,7 @@
 import Domain
 
 final class MockChatRepository: PChatRepository {
-    func getChats() async throws -> [Domain.ChatEntity] {
+    func getChats() async throws -> [ChatEntity] {
         // Generate 5-8 mock chats with realistic conversation data
         let chatCount = Int.random(in: 5...8)
         return MockDataGenerator.shared.generateRandomChats(count: chatCount)
