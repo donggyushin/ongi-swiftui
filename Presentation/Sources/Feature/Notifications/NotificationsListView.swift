51086c6803e2df2b6c28fa072d1d2fb0bcbda1bf
Loopy <loopy@heyratel.com>
2025-08-29 16:08:18 +0900

    add notificationsListView

---
+++ Presentation/Sources/Feature/Notifications/NotificationsListView.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsListView.swift
+//  Presentation
+//
+//  Created by 신동규 on 8/29/25.
+//
+
+import SwiftUI
+
+struct NotificationsListView: View {
+    
+    @StateObject var model: NotificationsListViewModel
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ Presentation/Sources/Feature/Notifications/NotificationsListViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  NotificationsListViewModel.swift
+//  Presentation
+//
+//  Created by 신동규 on 8/29/25.
+//
+
+import SwiftUI
+import Domain
+import Combine
+import Factory
+
+final class NotificationsListViewModel: ObservableObject {
+    
+    @Published var loading = false
+    @Published var notifications: NotificationsEntity = .init(notifications: [], nextCursor: nil, hasMore: true)
+    
+    @Injected(\.notificationsUseCase) private var notificationsUseCase
+    
+    @MainActor
+    func fetchNotifications() async throws {
+        guard notifications.hasMore == true else { return }
+        guard loading == false else { return }
+        
+        loading = true
+        defer { loading = false }
+        
+        let notifications = try await notificationsUseCase.getNotifications(cursorId: notifications.notifications.last?.id)
+        self.notifications = notifications
+    }
+}
