07a4a8a4e44f3dcd62e6a6ace7f01f4585704f2b
Loopy <loopy@heyratel.com>
2025-08-29 14:14:16 +0900

    chore

---
+++ DataSource/Sources/DataSources/Remote/NotificationsRemoteDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationsRemoteDataSource.swift
+//  DataSource
+//
+//  Created by 신동규 on 8/29/25.
+//
+
+import Foundation
+import Domain
+
+final class NotificationsRemoteDataSource {
+    let networkManager = NetworkManager.shared
+    
+}
