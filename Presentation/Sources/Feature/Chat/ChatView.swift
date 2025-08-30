6edc0577160abf7718605285b5e0fdd5bad7a1f5
신동규 <donggyu9410@gmail.com>
2025-08-30 23:18:29 +0900

    seperate code

--- Presentation/Sources/Feature/Chat/ChatView.swift
+++ Presentation/Sources/Feature/Chat/ChatView.swift
@@ -319,52 +319,6 @@ struct LeaveChatMessage: View {
     }
 }
 
-struct ChatSidebar: View {
-    let onLeaveChatTap: () -> Void
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            // Header
-            HStack {
-                Text("메뉴")
-                    .pretendardTitle3(.semiBold)
-                    .foregroundColor(.primary)
-                Spacer()
-            }
-            .padding(.horizontal, 20)
-            .padding(.top, 20)
-            .padding(.bottom, 16)
-            
-            Divider()
-            
-            // Menu Items
-            VStack(spacing: 0) {
-                Button(action: onLeaveChatTap) {
-                    HStack(spacing: 12) {
-                        Image(systemName: "rectangle.portrait.and.arrow.right")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.red)
-                        
-                        Text("채팅방 나가기")
-                            .pretendardBody(.medium)
-                            .foregroundColor(.red)
-                        
-                        Spacer()
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 16)
-                }
-                .background(Color(.systemBackground))
-            }
-            
-            Spacer()
-        }
-        .frame(width: 280)
-        .background(Color(.systemBackground))
-        .shadow(color: Color.black.opacity(0.1), radius: 10, x: -5, y: 0)
-    }
-}
-
 #if DEBUG
 private struct ChatViewPreview: View {
     
