83fe76e48cb996a28ba4199fdb32c8a35e4b6b5f
신동규 <donggyu9410@gmail.com>
2025-09-06 19:39:27 +0900

    chore

--- Project.swift
+++ Project.swift
@@ -70,7 +70,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleDisplayName": "온기",
-                    "CFBundleVersion": "7",
+                    "CFBundleVersion": "8",
                     "CFBundleShortVersionString": "1.0.0",
                     "CFBundleURLTypes": [
                         [
